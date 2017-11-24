c01e8c3ec38c0c71d68f13d6a37d3c6f29291d8b
petez <pete.zsolt@codeyard.eu>
2017-11-24 21:01:18 +0100

    Initial Commit

---
+++ AlertManager/AlertManager.h
@@ -0,0 +1,19 @@
+//
+//  AlertManager.h
+//  AlertManager
+//
+//  Created by Zsolt Pete on 2017. 11. 24..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertManager.
+FOUNDATION_EXPORT double AlertManagerVersionNumber;
+
+//! Project version string for AlertManager.
+FOUNDATION_EXPORT const unsigned char AlertManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertManager/PublicHeader.h>
+
+
