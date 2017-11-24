06acd638fab2dc0f6cb0349a528f51d94507f6d3
petez <pete.zsolt@codeyard.eu>
2017-11-24 23:33:16 +0100

    🚧 Make it public

--- AlertManager/AlertManager.swift
+++ AlertManager/AlertManager.swift
@@ -16,7 +16,7 @@ class AlertManager{
     private init(){
     }
     
-    func createPopUp(alert: Alert) -> UIAlertController{
+    public func createPopUp(alert: Alert) -> UIAlertController{
         let alertViewController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: .alert)
         alert.actions.forEach { (option) in
             let buttonAction = UIAlertAction(title: option.title, style: .default, handler: { (alert) in
